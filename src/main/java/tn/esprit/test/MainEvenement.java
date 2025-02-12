650834974d8ee9e0a1756935540f5984d98d3443
Adouani Feres <feresadouani10@gmail.com>
2025-02-12 15:57:51 +0100

    correction Main

--- src/main/java/tn/esprit/test/MainEvenement.java
+++ src/main/java/tn/esprit/test/MainEvenement.java
@@ -12,30 +12,24 @@ public class MainEvenement {
     public static void main(String[] args) {
         EvenementService es = new EvenementService();
         CategorieEvService ces = new CategorieEvService();
-           Categorieevent ce = new Categorieevent(1,"c est un evenement Sportif","Sport");
-           ces.add(ce);
-            System.out.println(ces.getAll());
-            System.out.println("Categorie event Inserer");
-            ces.delete(ce);
-            int categorieId = -1;
-            for (Categorieevent c : ces.getAll()) {
-                if (c.getNom().equals("Rendonner")) {
-                    categorieId = c.getId();
-                    break;
-                }
-            }
-            Evenement e = new Evenement(1,categorieId, 1000, "Evenement de Sport", "Stade de Tunis", Date.valueOf("2025-03-20"));
-            es.add(e);
+        Categorieevent ce = new Categorieevent("c est un evenement Sportif","Sport");
+        ces.add(ce);
+        System.out.println(ces.getAll());
+        System.out.println("Categorie event Inserer");
+        // ces.delete(ce);
 
-            es.update(e);
-            es.delete(e);
-            ces.update(ce);
+        Evenement e = new Evenement(1,7, 1000, "Evenement de Sport", "Stade de Tunis", Date.valueOf("2025-03-20"));
+        es.add(e);
 
-            System.out.println("Événement mis à jour avec succès !");
-            System.out.println(es.getAll());
+        //es.update(e);
+        //es.delete(e);
+        //ces.update(ce);
 
-                //Recherche par nom d'événement
-            String rechercheNom = "Sport";
+        //System.out.println("Événement mis à jour avec succès !");
+        //System.out.println(es.getAll());
+
+        //Recherche par nom d'événement
+        /*    String rechercheNom = "Evenement de Sport";
             List<Evenement> evenementsTrouves = es.rechercheByNom(rechercheNom);
             if (!evenementsTrouves.isEmpty()) {
                 System.out.println("Événements trouvés pour la recherche : " + rechercheNom);
@@ -44,9 +38,10 @@ public class MainEvenement {
                 }
             } else {
                 System.out.println("Aucun événement trouvé pour : " + rechercheNom);
-            }
+            }*/
 
         //Recherche par nom de catégorie
+        String rechercheNom = "Sport";
         List<Categorieevent> lce = ces.rechercheByNom(rechercheNom);
         if (!lce.isEmpty()) {
             System.out.println("Catehorie trouvés pour la recherche : " + rechercheNom);
@@ -57,12 +52,12 @@ public class MainEvenement {
             System.out.println("Aucun événement trouvé pour : " + rechercheNom);
         }
 
-            boolean succes = es.reserverPlace(2,2);
+         /*   boolean succes = es.reserverPlace(2,7);
             if(succes){
                 System.out.println("Place reservé");}
             else{
                 System.out.println("Place non reservé");
-            }
+            }*/
 
     }
 }
